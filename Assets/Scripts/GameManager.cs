d4006dfc2f2696dd50e63f51e673810500f18de1
jaidev14 <jaimeroma97@gmail.com>
2023-06-22 15:35:45 +0200

    Maze basic generation

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private Maze mazePrefab;
+    private Maze mazeInstance;
+    private void Start()
+    {
+        BeginGame();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R)) {
+            RestartGame();
+        }
+    }
+
+    private void BeginGame() {
+        mazeInstance = Instantiate(mazePrefab) as Maze;
+        // mazeInstance.Generate();
+        StartCoroutine(mazeInstance.GenerateRandomWithDelay());
+    }
+    private void RestartGame() {
+        StopAllCoroutines();
+        Destroy(mazeInstance.gameObject);
+        BeginGame();
+    }
+}
---
+++ Assets/Scripts/MazeCell.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class MazeCell : MonoBehaviour
+{
+    public IntVector2 coordinates;
+}
